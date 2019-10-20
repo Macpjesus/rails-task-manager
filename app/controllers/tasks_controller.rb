7176620a0e73a19132bd6aaa452e2a0524434bdb
Marco Jesus <macpjesus@gmail.com>
2019-10-20 18:18:53 -0300

    solved rails-task-manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,13 @@
 class TasksController < ApplicationController
+
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -19,17 +22,17 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to tasks_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    # @task = Task.find(params[:id])
     @task.destroy
     # no need for app/views/restaurants/destroy.html.erb
     redirect_to tasks_path
@@ -41,5 +44,9 @@ class TasksController < ApplicationController
     # *Strong params*: You need to *whitelist* what can be updated by the user
     # Never trust user data!
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
 
-  # READ
-  get 'tasks', to: 'tasks#index'
 
-  # CREATE
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  # # READ
+  # get 'tasks', to: 'tasks#index'
 
-  # READ
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # # CREATE
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-  # UPDATE
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
-  patch 'tasks/:id', to: 'tasks#update'
+  # # READ
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  # DELETE
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # # UPDATE
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # DELETE
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
